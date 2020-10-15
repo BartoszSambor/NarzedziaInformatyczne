b797bcca382591d5ce907e0ba4f2832750cc3c56
Bartosz Sambór <72545684+BartoszSambor@users.noreply.github.com>
2020-10-15 15:06:44 +0200

    Add files via upload

---
+++ Zadanie5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,ok;
+    ok = scanf("%d",&a);
+    if(ok==1){
+        printf("OK");
+    }else{
+         printf("Nie OK");
+    }
+}
